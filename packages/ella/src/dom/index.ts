939cfda796975661c8d0454f1252545f3d555cae
aditya vandan sharma <aditya.sharma@groww.in>
2022-03-02 12:31:14 +0530

    dataURIToBlob added to ella

--- packages/ella/src/dom/index.ts
+++ packages/ella/src/dom/index.ts
@@ -661,3 +661,44 @@ function reloadOnBackForward(event: PageTransitionEvent) {
     window.location.reload();
   }
 }
+
+
+/**
+ * Converts base64/URLEncoded data component to raw binary data held in a string and returns the raw binary string
+ *
+ * @param {string} dataURI base64/URLEncoded data component
+ *
+ * @remarks
+ * This method is only used is places where we are using canvas to get an esign.
+ * stackoverflow thread for context: https://stackoverflow.com/questions/12168909/blob-from-dataurl
+ *
+ * @returns {Blob} raw binary data blob
+ */
+ export function dataURIToBlob(dataURI: string) {
+  try {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    let byteString;
+
+    if (dataURI.split(',')[ 0 ].indexOf('base64') >= 0) {
+      byteString = window.atob(dataURI.split(',')[ 1 ]);
+
+    } else {
+      byteString = window.unescape(dataURI.split(',')[ 1 ]);
+    }
+
+    // separate out the mime component
+    const mimeString = dataURI.split(',')[ 0 ].split(':')[ 1 ].split(';')[ 0 ];
+
+    // write the bytes of the string to a typed array
+    const ia = new Uint8Array(byteString.length);
+
+    for (let i = 0; i < byteString.length; i++) {
+      ia[ i ] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ ia ], { type: mimeString });
+
+  } catch (error) {
+    return new Blob();
+  }
+}
