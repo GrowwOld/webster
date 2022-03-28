244e456a2db62fdc1e55e9d25687c6e498af7c38
Sarthak <sarthak.srivastava@groww.in>
2022-03-28 15:13:26 +0530

    ella/refactor

--- packages/ella/src/general/index.ts
+++ packages/ella/src/general/index.ts
@@ -467,7 +467,7 @@ export function getPathVariableFromUrlIndex(url: string, indexFromLast: number =
  *
  * @param {GenericFunction} func - Method that needs to be debounced
  * @param {number} delay - Amount of delay in miliseconds
- * @param {boolean} leading - 
+ * @param {boolean} leading - A boolean value for if the debounce functions is leading
  * * @example
  * ```
  * export function Input() {
