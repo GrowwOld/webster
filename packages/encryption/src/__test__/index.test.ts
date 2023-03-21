c41169a6fa085c6af786e7f1dc3a68d9b9e6d09b
sandeep <sandeepisthename007@gmail.com>
2023-03-21 23:55:33 +0530

    doc updated for test cases

--- packages/encryption/src/__test__/index.test.ts
+++ packages/encryption/src/__test__/index.test.ts
@@ -1,6 +1,9 @@
 import { expect, test } from 'vitest';
 import { encryptAes, decryptAes } from '../index';
 
+
+//This secret key is just for testing purpose
+// eslint-disable-next-line
 const SECRET_KEY = 'TEST_SECRET_KEY';
 
 const stringToEncrypt = 'some random string';
@@ -10,12 +13,25 @@ const objectToEncrypt = {
   age: 25
 };
 
+/*
+We are passing the two arguments to our test cases
+1. The data that needs to be encrypted
+2. The response from encryptAes function which in turn gives us object with keys {data and error}
+
+
+We are then passing the data from response from encryptAes function to decryptAes function which
+in turn gives us object with keys {data and error}
+
+We are then comparing the data from the response of decryptAes function with the original data
+
+We are also checking if the error key is null in both the response objects from encryptAes
+*/
 test.each([
   [ stringToEncrypt, encryptAes(stringToEncrypt, SECRET_KEY) ],
   [ objectToEncrypt, encryptAes(objectToEncrypt, SECRET_KEY) ]
 
 
-])('Check if decrption of the encrypted data is equal to the original data',
+])('Check if decryption of the encrypted data is equal to the original data',
   (expected, encryptedApiResponse) => {
     const decryptApiResponse = decryptAes(encryptedApiResponse.data, SECRET_KEY);
 
