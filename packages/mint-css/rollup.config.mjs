a514eb2ad4453806809e35d1bddbf884d82add27
shubhamgavali1998 <113414130+shubhamgavali1998@users.noreply.github.com>
2023-07-05 15:38:10 +0530

    fix: typography almost done

---
+++ packages/mint-css/rollup.config.mjs
@@ -0,0 +1,45 @@
+import postcssModules from 'rollup-plugin-postcss-modules';
+import postcssImport from 'postcss-import';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: './index.js', // Entry point of your application
+  output: {
+    file: './dist/bundle.css', // Output file
+    format: 'cjs', // Output format (can be 'iife', 'umd', 'cjs', 'esm')
+  },
+  plugins: [
+    postcssModules({
+      plugins: [
+        postcssImport(),
+      ],
+      extract: true,
+      minimize: true,
+      sourceMap: false,
+      modules: true,
+      loaders: [
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: 'url-loader',
+        },
+      ],
+    }),
+    copy({
+      targets: [
+        { src: './typography/fonts/*.woff2', dest: 'dist/fonts' }, // Copy font files to the desired output directory
+      ],
+      after: [
+        {
+          src: 'dist/bundle.css',
+          dest: 'dist',
+          transform: (contents) => {
+            // Modify the font paths in the CSS file
+            const modifiedContents = contents.toString().replace(/url\(fonts\//g, 'url(dist/fonts/');
+            return modifiedContents;
+          },
+        },
+      ],
+    }),
+  ],
+
+};
