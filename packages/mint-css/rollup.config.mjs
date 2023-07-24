23b6bf0cfa79b4b907aa2c90b9e8fa85521b3481
shubhamgavali1998 <113414130+shubhamgavali1998@users.noreply.github.com>
2023-07-24 14:18:17 +0530

    fix: update rollup config and change tertiary button color

--- packages/mint-css/rollup.config.mjs
+++ packages/mint-css/rollup.config.mjs
@@ -1,15 +1,14 @@
-import postcssModules from 'rollup-plugin-postcss';
+import postcss from 'rollup-plugin-postcss';
 import postcssImport from 'postcss-import';
 import copy from 'rollup-plugin-copy';
 
 export default {
-  input: './index.css', // Entry point of your application
+  input: './index.css',
   output: {
     file: './dist/index.css', // Output file
-    format: 'cjs', // Output format (can be 'iife', 'umd', 'cjs', 'esm')
   },
   plugins: [
-    postcssModules({
+    postcss({
       plugins: [
         postcssImport(),
       ],
