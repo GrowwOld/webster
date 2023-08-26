778e9fdb10e9cabc934176093b81884df6e397a0
Abhishek Jha <abhishek.jha@groww.in>
2023-08-27 00:53:23 +0530

    fixed icons

--- packages/icon-store/scripts/generateTypes.js
+++ packages/icon-store/scripts/generateTypes.js
@@ -12,20 +12,21 @@
  
 
  function generateTypesForIconComponent() {
-     const componentFolderPaths = ['mi', 'custom'];
+     const componentFolderPaths = ['mi/cjs', 'custom/cjs'];
  
      componentFolderPaths.forEach(componentFolderPath => {
          console.log(chalk.green('Generating types for: ') + chalk.yellow(componentFolderPath));
+
  
          const components = getDirContent(componentFolderPath);
          
          const allComponentTypeLines = [`import { ReactIconComponentType } from "../types"`];
          
-         const declarationPath = `${componentFolderPath}/index.d.ts`;
+         const declarationPath = `${componentFolderPath.split('/')[0]}/index.d.ts`;
  
          components.forEach(component => {
- 
-             if(component.includes('.js') && !component.includes('index')){
+
+             if(component.includes('.js') && !component.includes('index') && !component.includes('utils')){
                  
                  const componentName = component.slice(0 , -3);
                  const contentForComponent = `export declare const ${componentName}: ReactIconComponentType;`
