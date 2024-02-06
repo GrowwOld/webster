86c30b7ebff1bf6d49be1e1e94bef263a9702e03
Saheb Giri <sahebg@groww.in>
2024-02-06 15:25:50 +0530

    fix: progress-bar text alignment and background color

--- packages/ui-toolkit/stories/ProgressBar.stories.tsx
+++ packages/ui-toolkit/stories/ProgressBar.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Story } from "@storybook/react";
+import { Story } from '@storybook/react';
 
 import { ProgressBar } from '../src/components/atoms';
-import { Props as ProgressBarProps } from '../src/components/atoms/ProgressBar';
+import { Props as ProgressBarProps } from '../src/components/atoms/ProgressBar/ProgressBar';
 
 export default {
   title: 'ProgressBar',
@@ -10,17 +10,35 @@ export default {
 };
 
 
-export const Template: Story<ProgressBarProps> = (args) => {
+const Template: Story<ProgressBarProps> = (args) => {
   return (
-    <div style={{margin: 24}}>
-      <ProgressBar completedValue={30} {...args} />
+    <div style={{ margin: 24 }}>
+      <ProgressBar {...args} />
     </div>
   );
-}
+};
 
-Template.args = {
-  name: "Demo ProgressBar",
-  color: "var(--purple500)",
-  fillerThickness: 10,
-  size: 420
-}
+export const Linear = Template.bind({});
+Linear.args = {
+  name: 'Linear Progress bar',
+  color: 'var(--purple500)',
+  fillerThickness: 8,
+  containerThickness: 5,
+  backgroundColor: 'var(--gray150)',
+  size: 420,
+  completedValue: 80
+};
+
+export const Circular = Template.bind({});
+Circular.args = {
+  name: 'Cicular progress bar',
+  isCircular: true,
+  color: 'var(--green500)',
+  backgroundColor: 'var(--gray150)',
+  text: 'Hello',
+  addTextClass: 'contentAccent',
+  fillerThickness: 6,
+  containerThickness: 2,
+  size: 100,
+  completedValue: 50
+};
