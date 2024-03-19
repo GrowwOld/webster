73cfafb029bc3ad8fb1bdc49f2cb152a2c3c9667
rajvarsani-groww <158473187+rajvarsani-groww@users.noreply.github.com>
2024-03-19 16:26:47 +0530

    lint(rbgroup): remove prettier liniting

--- packages/ui-toolkit/stories/RadioButtonGroup.stories.tsx
+++ packages/ui-toolkit/stories/RadioButtonGroup.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Story } from "@storybook/react";
+import { Story } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import { Props as RadioButtonGroupProps } from '../src/components/molecules/RadioButtonGroup/RadioButtonGroup';
@@ -7,49 +7,54 @@ import { RadioButtonGroup } from '../src/components/molecules';
 
 export default {
   title: 'RadioButtonGroup',
-  component: RadioButtonGroup,
+  component: RadioButtonGroup
 };
 
+
 const Template: Story<RadioButtonGroupProps> = (args) => {
-  const [value, setValue] = useState("");
+  const [ value, setValue ] = useState('');
+
 
   const onSelect = (newValue) => {
     if (newValue === value) {
-      setValue("");
+      setValue('');
 
     } else {
       setValue(newValue);
 
     }
-  }
+  };
 
   return (
     <div className="valign-wrapper">
-      <RadioButtonGroup {...args} selected={value} onSelect={onSelect} />
+      <RadioButtonGroup {...args}
+        selected={value}
+        onSelect={onSelect}
+      />
     </div>
-  )
+  );
 };
 
 export const Default = Template.bind({});
 
 const genderArray = [
-  { label: "Male", value: "MALE" },
-  { label: "Female", value: 'FEMALE', parentClassName: "bas11RadioParent" },
-  { label: "Other", value: "NA", parentClassName: "bas11RadioParent" }
+  { label: 'Male', value: 'MALE' },
+  { label: 'Female', value: 'FEMALE', parentClassName: 'bas11RadioParent' },
+  { label: 'Other', value: 'NA', parentClassName: 'bas11RadioParent' }
 ];
 
 Default.args = {
-  containerClassName: "",
-  radioButtons: genderArray,
+  containerClassName: '',
+  radioButtons: genderArray
 };
 
 export const Custom = Template.bind({});
 
 const switchArray = [
-  { label: "On", value: "ON", radioDirection: 'Right' },
-  { label: "Off", value: 'OFF', parentClassName: "bas11RadioParent" }
+  { label: 'On', value: 'ON', radioDirection: 'Right' },
+  { label: 'Off', value: 'OFF', parentClassName: 'bas11RadioParent' }
 ];
 
 Custom.args = {
-  radioButtons: switchArray,
-}
+  radioButtons: switchArray
+};
